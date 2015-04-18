5552a2a7c23fc38f0c5f18aba613ba6a4e4d1d52
shadowfiles <zhangtiff@gmail.com>
2015-04-17 22:11:17 -0400

    Functionality boilerplater.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 window.onload = function () {
 	// The functionality to be run when the when the page is loaded. 
 }
+
+function Sudoku (p) {
+	var puzzle = p;
+	this.checkValidity = function (row, col) {
+		
+	}
+}

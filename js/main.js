d5f0a4f361b6f9a23fc92f2035b445c753df40cc
shadowfiles <zhangtiff@gmail.com>
2015-04-18 23:45:09 -0400

    Hmm

--- js/main.js
+++ js/main.js
@@ -2,9 +2,17 @@ window.onload = function () {
 	// The functionality to be run when the when the page is loaded. 
 }
 
+function readPuzzle () {
+
+}
+
+function showSolution () {
+	
+}
+
 function Sudoku (p) {
 	var puzzle = p;
 	this.checkValidity = function (row, col) {
-		
+
 	}
 }
